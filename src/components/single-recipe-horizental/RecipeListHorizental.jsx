114c4f9ba01f5ddbb748e716b15adaed8c9ca5b9
Ahmad Mujtaba <ahmad.mujtaba@newbuffers.com>
2023-12-18 19:39:27 +0500

    feat: changes made on api request

--- src/components/single-recipe-horizental/RecipeListHorizental.jsx
+++ src/components/single-recipe-horizental/RecipeListHorizental.jsx
@@ -1,28 +1,42 @@
-import React from "react";
-import "../../shared/navigationbar.css"
+import React, { useState, useEffect } from "react";
+import "../../shared/navigationbar.css";
+import { getRecipesRandom } from "../../server/requests" ; 
 
 const RecipeListHorizental = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const response = await getRecipesRandom(4);
+        setRecipes(response.data);
+      } catch (error) {
+        console.error("Error fetching recipes:", error);
+      }
+    };
+
+    fetchRecipes();
+  }, []);
+
   return (
     <div>
-      RecipeListHorizental
-      <ul class="cat-list clearfix">
-        <li>
-          <a href="recipe-single-1.html" class="img-box">
-            <img
-              src="../../src/assets/images/demo/7a0a46455c4ec56a5a02c097374fc513-222x144.jpg"
-              class="attachment-recipe-4column-thumb wp-post-image"
-              alt="7a0a46455c4ec56a5a02c097374fc513"
-            />
-          </a>
-          <h4>
-            <a href="recipe-single-1.html"> Chocolate Earl Grey Pots... </a>
-          </h4>
-          <p>
-            {" "}
-            2 cups cream 120 grams dark chocolate, chopped 2 bags of earl grey
-            tea 6 <a href="recipe-single-1.html">...more </a>{" "}
-          </p>
-        </li>
+      <ul className="cat-list clearfix">
+        {
+        recipes.map((recipe) => (
+          <li key={recipe.id}>
+            <a href={`recipe-single-${recipe.id}.html`} className="img-box">
+              <img
+                src={recipe.image} // Assuming your API provides an 'image' property
+                className="attachment-recipe-4column-thumb wp-post-image"
+                alt={recipe.title}
+              />
+            </a>
+            <h4>
+              <a href={`recipe-single-${recipe.id}.html`}>{recipe.title}</a>
+            </h4>
+            <p>{recipe.ingredients}</p>
+          </li>
+        ))}
       </ul>
     </div>
   );
--- src/server/requests.js
+++ src/server/requests.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getRecipes = async () => {
     try {
-         return await axios.get(`${import.meta.env.API_BASE_URLapi/v1/recipe}`)
+         return await axios.get('http://localhost:8080/api/v1/recipe')
     } catch (error) {
         throw error;
     }
@@ -10,7 +10,7 @@ export const getRecipes = async () => {
 }
 export const getNews = async () => {
     try {
-         return await axios.get(`${import.meta.env.API_BASE_URL}/api/v1/news`)
+         return await axios.get('http://localhost:8080/api/v1/news')
     } catch (error) {
         throw error;
     }
@@ -18,13 +18,39 @@ export const getNews = async () => {
 }
 export const getBlogs = async () => {
     try {
-         return await axios.get(`${import.meta.env.API_BASE_URL}/api/v1/blog`)
+         return await axios.get('http://localhost:8080/api/v1/blog')
     } catch (error) {
         throw error;
     }
 
 }
 
+export const getRecipesRandom = async (random) => {
+    try {
+         return await axios.get(`http://localhost:8081/api/v1/recipe/${random}`)
+    } catch (error) {
+        throw error;
+    }
+
+}
+export const getNewsRandom = async (random) => {
+    try {
+         return await axios.get(`http://localhost:8080/api/v1/news/${random}`)
+    } catch (error) {
+        throw error;
+    }
+
+}
+export const getBlogRandom = async (random) => {
+    try {
+         return await axios.get(`http://localhost:8080/api/v1/blog/${random}`)
+    } catch (error) {
+        throw error;
+    }
+
+}
+
+// TODO: complete all the functionlity under here
 export const saveCustomer = async (customer) => {
     try {
         return await axios.post(`${import.meta.env.API_BASE_URL}/api/v1/customer/register`, customer)
